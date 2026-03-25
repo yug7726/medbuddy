6c2b44a36143cf76b019625d62561aaf33b4670d
yug7726 <yug13066@gmail.com>
2026-03-26 00:33:56 +0530

    Create App component with welcome message

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to MedBuddy</h1>
+      <p>Your Health Companion App</p>
+    </div>
+  )
+}
+
+export default App
